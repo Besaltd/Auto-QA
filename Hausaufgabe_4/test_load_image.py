d503bd00146fa67e729fbab6e8e1be53cdb29723
Ruslan <mellow.neliph@gmail.com>
2026-05-19 09:47:40 +0200

    Hausaufgabe_4 updated

--- Hausaufgabe_4/test_load_image.py
+++ Hausaufgabe_4/test_load_image.py
@@ -1,5 +1,21 @@
-def main():
-    pass
-    
-if __name__ == '__main__';
-    main()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pytest
+from time import sleep
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+    yield driver
+    driver.quit()
+
+def test_load_image(driver):
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#landscape")))
+    images = driver.find_elements(By.CSS_SELECTOR, "#image-container img")
+    image_3 = images[2]
+    assert image_3.get_attribute("alt").strip() == "award"
--- Hausaufgabe_4/test_text_btn.py
+++ Hausaufgabe_4/test_text_btn.py
@@ -1,5 +1,21 @@
-def main():
-    pass
-    
-if __name__ == '__main__';
-    main()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("http://uitestingplayground.com/textinput")
+    yield driver
+    driver.quit()
+
+
+def test_text_btn(driver):
+    input_field = driver.find_element(By.CSS_SELECTOR, "#newButtonName")
+    blue_btn = driver.find_element(By.CSS_SELECTOR, "#updatingButton")
+    input_field.send_keys("ITCH")
+
+    blue_btn.click()
+    assert blue_btn.text == "ITCH"
