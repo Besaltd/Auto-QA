f93051d77521fabe0d42b82512fad952e28bc360
Ruslan <mellow.neliph@gmail.com>
2026-05-19 09:52:06 +0200

    Arbeite an Hausaufgabe_5, nicht beendet

--- Hausaufgabe_5/test_drag_n_drop.py
+++ Hausaufgabe_5/test_drag_n_drop.py
@@ -1,5 +1,45 @@
-def main():
-    pass
-    
-if __name__ == '__main__';
-    main()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import pytest
+from time import sleep
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.globalsqa.com/demo-site/draganddrop/")
+    yield driver
+    driver.quit()
+
+
+def test_drag_and_drop(driver):
+
+    # Закриваємо GDPR попап
+    try:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Einwilligen') or contains(., 'Accept') or contains(., 'Zustimmen')]"))).click()
+    except:
+        pass
+
+    iframe = driver.find_element(By.CSS_SELECTOR, ".demo-frame")
+    driver.switch_to.frame(iframe)
+
+    trash = driver.find_element(By.ID, "trash")
+    first_img = driver.find_element(By.XPATH, "//img[contains(@alt, 'The peaks of High Tatras')]")
+    gallery_images = driver.find_elements(By.CSS_SELECTOR, "#gallery li h5:before")
+
+    actions = ActionChains(driver)
+    actions.drag_and_drop(first_img, trash).release().perform()
+
+    for el in gallery_images:
+        print(el.text)
+
+    print(len(gallery_images))
+
+
+
+    assert len(gallery_images) == 3
--- Hausaufgabe_5/test_iframe.py
+++ Hausaufgabe_5/test_iframe.py
@@ -1,5 +1,34 @@
-def main():
-    pass
-    
-if __name__ == '__main__';
-    main()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pytest
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/iframes.html")
+    yield driver
+    driver.quit()
+
+def test_iframe(driver):
+    text = "semper posuere integer et senectus justo curabitur."
+    iframe = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[src='content.html']"))
+    )
+    driver.switch_to.frame(iframe)
+
+    paragraphs = driver.find_elements(By.TAG_NAME, "p")
+
+    found = False
+
+    for p in paragraphs:
+        actual = " ".join(p.text.split())
+
+        if text in actual:
+            found = True
+            break
+
+    assert found, f"Текст '{text}' не знайдено"
